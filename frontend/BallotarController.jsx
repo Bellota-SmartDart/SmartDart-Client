3fb5bf139cdf5b5948164537880e9748b23a5b0d
huinalam <huinalam@gmail.com>
2016-08-27 23:15:45 +0900

    BallotarController EventHandler

--- frontend/BallotarController.jsx
+++ frontend/BallotarController.jsx
@@ -13,18 +13,47 @@ class BallotarController {
         console.log('ballotar_start');
     }
 
-    timer_test() {        
-        console.log('timer_test');        
-        $.getJSON("./static/all.json", function (data) {
-            console.log('ballotar_start get');            
-            window.dartDart = data;
-            console.log(window);
+    timer_test() {
+        console.log('timer_test');
+        var controller = this;
+        $.getJSON("./static/all.json", (data) => {
+            console.log('ballotar_start get');
+            controller.dartDart = data;
+            console.log(controller);
         });
     }
 
     // all
     get_loop_status() {
-        this.interval = setInterval(this.timer_test, 1000);
+        var controller = this;
+        this.interval = setInterval(() => {
+            console.log('timer_test');
+            $.getJSON("./static/all.json", (data) => {
+                console.log('ballotar_start get');
+                // controller.dartDart = data;
+                // console.log(data);
+                controller.is_changed_status(controller, data);
+            });
+        }, 1000);
+    }
+
+    is_changed_status(controller, data) {
+        if (controller.changedEventHandler != null) {
+            var oldData = null;
+            if (controller.dartData == null) {
+                oldData = data;
+            }
+            
+            if (oldData.current.player != data.current.player) {
+                controller.changedEventHandler('player',data);
+            } else if (oldData.current.round != data.current.round) {
+                controller.changedEventHandler('round',data);
+            } else if (oldData.current.set != data.current.set) {
+                controller.changedEventHandler('set',data);
+            } else {
+                controller.changedEventHandler('continue',data);
+            }
+        }
     }
 
     stop_get_status() {
--- frontend/game/MainViewer.jsx
+++ frontend/game/MainViewer.jsx
@@ -3,14 +3,22 @@ import $ from 'jquery';
 import cookie from 'react-cookie';
 
 class MainViewr extends React.Component {
-    render() {
-        console.log('MainViewr render');
+    componentDidMount() {
         this.props.BallotarController.get_loop_status();
         var controller = this.props.BallotarController;
-        setTimeout(function () {
+
+        setTimeout(() => {
             console.log('stop_get_status');
             controller.stop_get_status();
         }, 5000);
+        controller.changedEventHandler = (ctr, data) => {
+            console.log(ctr);
+            console.log(data);
+        }
+    }
+
+    render() {
+        console.log('MainViewr render');        
         return (
             <div className="mainViewer">
                 반가워요~
